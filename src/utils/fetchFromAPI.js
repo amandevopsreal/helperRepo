2e636532e61dd8d4a06b48bb031abb5cd453a83a
Aman Kumar Mishra <aman@Amans-MacBook-Air.local>
2023-12-30 21:25:11 +0530

    works ends

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+    params: {
+        maxResults: 50,
+    },
+    headers: {
+        'X-RapidAPI-Key': '18d564ef80msh5870770023acc90p1f4c91jsn5e4eee6a3bdb',
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    },
+};
+
+export const fetchFromAPI = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+    return data;
+};
