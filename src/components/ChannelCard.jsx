427ac6d7ecc9db4ebf6ecbbab0866e0952ee9bc8
Aman Kumar Mishra <aman@Amans-MacBook-Air.local>
2023-12-31 23:49:09 +0530

    added search feature and channel page

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom"
 import { demoProfilePicture } from "../utils/constants"
 
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
 
     return (
         <Box sx={{
             boxShadow: "none", borderRadius: "20px", display: "flex", justifyContent: "center", alignItems: "center",
-            width: { xs: "356px", md: "320px" }, height: "326px", margin: "auto"
+            width: { xs: "356px", md: "320px" }, height: "326px", margin: "auto", marginTop
         }}>
             <Link to={`/channel/${channelDetail?.id?.channelId}`}>
                 <CardContent
